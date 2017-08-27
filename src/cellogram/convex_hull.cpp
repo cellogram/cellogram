840f1cbc95e31db1ce860cc61c08c3db8aae5864
Jérémie Dumas <jeremie.dumas@ens-lyon.org>
2017-08-27 17:22:49 -0400

    Start integrating prototype app.

---
+++ src/cellogram/convex_hull.cpp
@@ -0,0 +1,52 @@
+#include "convex_hull.h"
+#include <algorithm>
+#include <numeric>
+#include <geogram/basic/geometry.h>
+
+struct Compare {
+	const std::vector<GEO::vec2> &points;
+	int leftmost; // Leftmost point of the poly
+
+	Compare(const std::vector<GEO::vec2> &points) : points(points) { }
+
+	bool operator ()(int i1, int i2) {
+		if (i2 == leftmost) { return false; }
+		if (i1 == leftmost) { return true; }
+		GEO::vec2 u = points[i1] - points[leftmost];
+		GEO::vec2 v = points[i2] - points[leftmost];
+		double d = det(u, v);
+		return (d < 0 || (d == 0 && u.length2() < v.length2()));
+	}
+};
+
+bool inline salientAngle(const GEO::vec2 &a, const GEO::vec2 &b, const GEO::vec2 &c) {
+	return (det(b - a, c - a) >= 0);
+}
+
+std::vector<int> cellogram::convex_hull(const std::vector<GEO::vec2> &points) {
+	Compare order(points);
+	order.leftmost = 0;
+	for(int i = 1; i < (int) points.size(); ++i) {
+		if (points[i][0] < points[order.leftmost][0]) {
+			order.leftmost = i;
+		} else if (points[i][0] == points[order.leftmost][0]
+			&& points[i][1] < points[order.leftmost][1])
+		{
+			order.leftmost = i;
+		}
+	}
+	std::vector<int> index(points.size());
+	std::iota(index.begin(), index.end(), 0);
+	std::sort(index.begin(), index.end(), order);
+	std::vector<int> hull;
+	for (int i : index) {
+		hull.push_back(i);
+		while (hull.size() > 3u && salientAngle(points[hull.end()[-3]],
+			points[hull.end()[-2]], points[hull.end()[-1]]))
+		{
+			hull.end()[-2] = hull.back();
+			hull.pop_back(); // Pop inner vertices
+		}
+	}
+	return hull;
+}
--- src/viewer/main.cpp
+++ src/viewer/main.cpp
@@ -1,37 +1,126 @@
 ////////////////////////////////////////////////////////////////////////////////
+#include "cellogram/convex_hull.h"
 #include "viewer/file_dialog.h"
+#include <geogram/basic/file_system.h>
 #include <geogram/basic/command_line.h>
 #include <geogram/basic/command_line_args.h>
+#include <geogram/mesh/mesh.h>
+#include <geogram/mesh/mesh_io.h>
+#include <geogram_gfx/glup_viewer/glup_viewer.h>
 #include <geogram_gfx/glup_viewer/glup_viewer_gui.h>
+#include <geogram_gfx/mesh/mesh_gfx.h>
+////////////////////////////////////////////////////////////////////////////////
+
+typedef GEO::vec2 vec2;
+
+struct Scene {
+	std::vector<bool> fixed;
+	std::vector<vec2> detected;
+	std::vector<vec2> reference;
+	std::vector<vec2> border;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+namespace {
+
+	// Load vertices from a mesh file (mesh can be a .obj, .ply, .xyz, etc.)
+	std::vector<vec2> load_points(const std::string &filename) {
+		GEO::Mesh M;
+		if(!GEO::mesh_load(filename, M)) {
+			return {};
+		}
+		std::vector<vec2> points;
+		for (GEO::index_t i = 0; i < M.vertices.nb(); ++i) {
+			GEO::vec3 p = M.vertices.point(i);
+			points.push_back(vec2(p[0], p[1]));
+		}
+		return points;
+	}
+
+} // anonymous namespace
+
 ////////////////////////////////////////////////////////////////////////////////
 
 class CellogramApplication : public GEO::SimpleMeshApplication {
+	Scene scene;
+
+	bool show_detected_;
+	bool show_reference_;
+	bool show_border_;
+
 public:
-	CellogramApplication(int argc, char** argv, const std::string& usage) :
-		SimpleMeshApplication(argc, argv, "")
+	CellogramApplication(int argc, char** argv, const std::string& usage)
+		: SimpleMeshApplication(argc, argv, "")
+		, show_detected_(true)
+		, show_reference_(false)
+		, show_border_(true)
 	{
 		name_ = "[Float] Cellogram Viewer";
 	}
 
+	virtual void init_graphics() override {
+		SimpleMeshApplication::init_graphics();
+		glup_viewer_disable(GLUP_VIEWER_BACKGROUND);
+		glup_viewer_disable(GLUP_VIEWER_3D);
+	}
+
 	virtual void draw_load_menu() override {
-		if(ImGui::MenuItem("Load...")) {
-			std::string filename = FileDialog::openFileName();
+		if(ImGui::MenuItem("Load detected...")) {
+			std::string filename = FileDialog::openFileName(DATA_DIR);
+			load(filename);
 		}
 	}
 
+	virtual bool load(const std::string &filename) override {
+		if(!GEO::FileSystem::is_file(filename)) {
+			GEO::Logger::out("I/O") << "is not a file" << std::endl;
+			return false;
+		}
+		scene.detected = load_points(filename);
+		SimpleMeshApplication::load(filename);
+		current_file_ = "";
+		auto hull = cellogram::convex_hull(scene.detected);
+		scene.fixed.assign(scene.detected.size(), false);
+		scene.border.clear();
+		for (int x : hull) {
+			scene.fixed[x] = true;
+			scene.border.push_back(scene.detected[x]);
+		}
+		return true;
+	}
+
 	virtual void draw_save_menu() override {
-		if(ImGui::MenuItem("Save as...")) {
+		if(ImGui::MenuItem("Save...")) {
 			std::string filename = FileDialog::saveFileName();
 		}
 	}
 
-	/**
-	 * \brief Draws the application menus.
-	 * \details This function overloads
-	 *  Application::draw_application_menus(). It can be used to create
-	 *  additional menus in the main menu bar.
-	 */
-	virtual void draw_application_menus() {
+	virtual void draw_viewer_properties() override {
+		ImGui::Checkbox("detected points", &show_detected_);
+		ImGui::Checkbox("reference points", &show_reference_);
+		ImGui::Checkbox("border", &show_border_);
+	}
+
+	virtual void draw_scene() override {
+		SimpleMeshApplication::draw_scene();
+		if (show_border_) {
+			draw_border();
+		}
+	}
+
+	void draw_border() {
+		glupSetColor3f(GLUP_FRONT_AND_BACK_COLOR, 0.0, 0.0, 0.0);
+		glupSetMeshWidth(4);
+		glupBegin(GLUP_LINES);
+		for(GEO::index_t i=0; i < scene.border.size(); ++i) {
+			glupVertex(scene.border[i]);
+			glupVertex(scene.border[(i+1)%scene.border.size()]);
+		}
+		glupEnd();
+	}
+
+	void draw_reference() {
 
 	}
 
