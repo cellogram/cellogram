cb9b16180f0cf6a57fe91433cdb175746dbfe57f
Teseo Schneider <teseo@bluewin.ch>
2018-05-02 12:21:52 -0400

    added tif reader

---
+++ misc/tif_test.cpp
@@ -0,0 +1,17 @@
+
+#include <cellogram/tif_reader.h>
+#include <Eigen/Dense>
+#include <iostream>
+
+int main(int argc, char** argv) {
+	const std::string root = DATA_DIR;
+	const std::string filename = root + "cell.tif";
+
+
+	Eigen::Matrix<uint16_t, Eigen::Dynamic, Eigen::Dynamic> img;
+	cellogram::read_tif_image(filename, img);
+	std::cout<<img<<std::endl;
+
+
+	return 0;
+}
